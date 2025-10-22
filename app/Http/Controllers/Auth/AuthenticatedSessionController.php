7aeb7ed0b836994129543caeb8c8ab66ab6be034
bagus <bagus.pranowo@vnt.co.id>
2025-10-22 13:52:01 +0700

    passport: authorize, logout, refresh, login

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace App\Http\Controllers\Auth;
 
+use ArrayAccess;
 use Dedoc\Scramble\Attributes\Group;
+use Exception;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
+use Laravel\Passport\AccessToken;
+use Laravel\Passport\Contracts\ScopeAuthorizable;
+use Laravel\Passport\Passport;
+use Laravel\Passport\Token;
+use Log;
+use Symfony\Component\HttpFoundation\Response;
 
 #[Group('Authentication')]
 final class AuthenticatedSessionController extends Controller
@@ -36,13 +44,34 @@ final class AuthenticatedSessionController extends Controller
     /**
      * Logout
      */
-    public function logout(): JsonResponse
+    public function logout(Request $request): JsonResponse
     {
-        //
+        /**
+         * @var AccessToken<mixed>|null $currentToken
+         */
+        $currentToken = $request->user()?->token();
+
+        if ($currentToken === null) {
+            abort(Response::HTTP_UNAUTHORIZED, 'Unauthenticated.');
+        }
+
+        try {
+            $currentToken->revoke();
+        } catch (Exception $exception) {
+            Log::error($exception->getMessage());
+
+            return response()->json([
+                'message' => 'Internal Server Error',
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        return response()->json([
+            'message' => 'Logout success',
+        ]);
     }
 
     /**
-     * Refresh token session
+     * Refresh
      */
     public function refresh(Request $request): JsonResponse
     {
@@ -53,7 +82,8 @@ final class AuthenticatedSessionController extends Controller
         /** @var string $clientSecret */
         $clientSecret = config('passport.password_client_secret');
 
-        $response = Http::asForm()->post($baseUrl.'/api/vendor/passport/token', [
+        $url = $baseUrl.'/api/vendor/passport/token';
+        $response = Http::asForm()->post($url, [
             'grant_type' => 'refresh_token',
             'refresh_token' => $request->bearerToken(),
             'client_id' => $clientId,
@@ -62,7 +92,8 @@ final class AuthenticatedSessionController extends Controller
         ]);
 
         if ($response->failed()) {
-            return response()->json($response->body(), $response->status());
+            Log::error($response->getReasonPhrase());
+            abort(Response::HTTP_UNAUTHORIZED, 'Unauthorized.');
         }
 
         /** @var array<string, mixed>|null $jsonResponse */
--- routes/src/auth.php
+++ routes/src/auth.php
@@ -9,9 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:api'])->group(function (): void {
     Route::get('/authorize', [AuthenticatedSessionController::class, 'authorize']);
     Route::post('/logout', [AuthenticatedSessionController::class, 'logout']);
-    Route::post('/refresh', [AuthenticatedSessionController::class, 'refresh']);
 });
 
-Route::middleware(['guest'])->group(function (): void {
-    Route::post('/login', [LoginController::class, 'login']);
-});
+Route::post('/refresh', [AuthenticatedSessionController::class, 'refresh']);
+Route::post('/login', [LoginController::class, 'login']);
